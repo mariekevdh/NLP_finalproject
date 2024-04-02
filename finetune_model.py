fff115c0b5289e6b732183bbb1e40210a2101760
Marieke van der Holt <mariekevanderholt@gmail.com>
2024-04-02 22:43:08 +0200

    add docstrings and type hints

--- finetune_model.py
+++ finetune_model.py
@@ -1,19 +1,29 @@
-from datasets import load_dataset, concatenate_datasets, DatasetDict
-from transformers import (
-    AutoTokenizer,
-    AutoModelForSequenceClassification,
-    Trainer,
-    TrainingArguments,
-)
+import argparse
+import os
+from typing import Union
+
 import evaluate
 import numpy as np
 import torch
-import argparse
-import os
 import torch.nn.functional as F
+from datasets import Dataset, DatasetDict, load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+    Trainer,
+    TrainingArguments,
+)
 
 
-def create_arg_parser():
+def create_arg_parser() -> argparse.Namespace:
+    """
+    Creates and returns argument parser for command-line arguments.
+
+    Returns:
+        Namespace containing the command-line arguments as attributes.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-th",
@@ -98,46 +108,61 @@ def create_dataset(
     score_method: str = "da",
     qe_mix_da_weight: float = 0.5,
     language: str = "nl",
-    baseline: bool = False
+    baseline: bool = False,
 ) -> DatasetDict:
     """
-    This function returns a DatasetDict with the columns 'premise',
-    'hypothesis' and 'labels' in the following Datasets:
-    train: train data from the GroNLP/ik-nlp-22_transqe dataset
-    validation: validation and test data form the GroNLP/ik-nlp-22_transqe dataset
-    test: train, validation and test data from the maximedb/sick_nl dataset.
+    Loads and processes the GroNLP/ik-nlp-22_transqe and maximedb/sick_nl dataset from Huggingface.
+    The GroNLP/ik-nlp-22_transqe dataset can be filtered on the parameters specified below.
+    The evalutaion part of the maximedb/sick_nl will be added as evalutation data.
+    If the parameter baseline is True, the train part of this dataset will be added as train data.
 
-    If the language is 'nl', a column 'qe' will be added to the train Dataset, consisting of either
-    the da-score, mqm-score or a mix both scores from the GroNLP/ik-nlp-22_transqe dataset.
-    For the da- and mqm-score the lowest of the hypothesis and premise values is chosen.
+    Parameters:
+        qe_threshold (float, optional): The minimum quality estimation score
+            for including an example in the dataset. The lowest score between
+            the premise- and hypothesis-score will be used for filtering.
+            Defaults to 0.0, which includes all examples.
+        score_method (str, optional): The scoring method to use. Can be 'da' (Direct Assessment),
+            'mqm' (Multidimensional Quality Metrics), or 'mix' for a weighted mix of 'da' and 'mqm' scores.
+            Defaults to 'da'. 'mqm' will be scaled to a 0-1 scale to match 'da'.
+        qe_mix_da_weight (float, optional): The weight for the 'da' score in the mixed score calculation.
+            Only relevant if `score_method` is set to 'mix'. Defaults to 0.5.
+        language (str, optional): The language of the dataset to load. Filtering is only possible for 'nl' (Dutch).
+            Defaults to 'nl'.
+        baseline (bool, optional): If True, adds a baseline dataset (SICK) as train data.
 
-    The data can be filtered on the da-score, mqm-score or a mix both scores
-    from the GroNLP/ik-nlp-22_transqe dataset. Can return either Dutch or English data.
-
-    Arguments:
-    qe_threshold (float, optional): Quality Estimation score threshold. A float between 0 and 1 (default 0.0).
-    score_method (str, optional): Type of qe score used for filtering. Can be 'da', 'mqm', or 'mix' (default 'da').
-    qe_mix_da_weight (float, optional): Weight of 'da' score in mixed qe score. A float between 0 and 1 (default 0.5).
-    language (str, optional): Language of training data. Can be 'nl' or 'en' (default 'nl').
-
-    Returns: DatasetDict with three Datasets: train, validation and test.
+    Returns:
+        DatasetDict: The processed dataset filtered using the specified parameters.
+        Contains a Dataset called 'train' and a Dataset called 'validation'.
     """
 
     score_type = score_method
-    def lowest_qe(example):
+
+    def lowest_qe(example: dict) -> dict:
+        """
+        Calculates the minimum QE score between premise and hypothesis
+        scores in the given example and adds it to the example under the 'qe' key.
+        """
         premise = float(example["{}_premise".format(score_type)])
         hypothesis = float(example["{}_hypothesis".format(score_type)])
         example["qe"] = min(premise, hypothesis)
         return example
 
-    def scale_mqm(example):
+    def scale_mqm(example: dict) -> dict:
+        """
+        Scales the mqm score in an example to a 0-1 scale.
+        """
         example["mqm_premise"] = float(example["mqm_premise"]) / 0.2
         example["mqm_hypothesis"] = float(example["mqm_hypothesis"]) / 0.2
         return example
 
     qe_mix_da_weight = float(qe_mix_da_weight)
 
-    def mix_scores(example):
+    def mix_scores(example: dict) -> dict:
+        """
+        Calculates a mixed score for both premise and hypothesis by combining da and mqm scores
+        based on a specified weight for the da score in each example. Adds the mixed scores as
+        'mix_premise' and 'mix_hypothesis' to the example.
+        """
         example["mix_premise"] = float(
             example["da_premise"]
         ) * qe_mix_da_weight + float(example["mqm_premise"]) * (1 - qe_mix_da_weight)
@@ -146,17 +171,22 @@ def create_dataset(
         ) * qe_mix_da_weight + float(example["mqm_hypothesis"]) * (1 - qe_mix_da_weight)
         return example
 
-    def swap_values(example):
-        if example['label'] == 0:
-            example['label'] = 2
-        elif example['label'] == 2:
-            example['label'] = 0
+    def swap_values(example: dict) -> dict:
+        """
+        Swaps the labels 0 and 2 in a given example. At the time of writing,
+        this is needed for the SICK-NL dataset to match the TransQE dataset.
+        """
+        if example["label"] == 0:
+            example["label"] = 2
+        elif example["label"] == 2:
+            example["label"] = 0
         return example
 
-    # Load in base dataset
+    # Load in base datasets
     dataset = load_dataset("GroNLP/ik-nlp-22_transqe")
     dataset_sicknl = load_dataset("maximedb/sick_nl")
 
+    # Rename columns in sick dataset to match the transqe dataset
     dataset_sicknl = dataset_sicknl.rename_columns(
         {
             "sentence_A": "premise_nl",
@@ -166,18 +196,24 @@ def create_dataset(
         }
     )
 
+    # Swap the labels 0 and 2 in the sick dataset
     dataset_sicknl = dataset_sicknl.map(swap_values)
 
     if language == "nl":
         # Scale mqm score to a 0-1 scale
         dataset["train"] = dataset["train"].map(scale_mqm)
         if score_type == "mix":
+            # Add mixed qe scores
             dataset["train"] = dataset["train"].map(mix_scores)
+        # Get lowest qe scores between premise and hypothesis
         dataset["train"] = dataset["train"].map(lowest_qe)
     if baseline:
+        # Add sick data as train data
         dataset["train"] = dataset_sicknl["train"]
+    # Add sick data as validation data
     dataset["validation"] = dataset_sicknl["validation"]
 
+    # Filter the dataset on the given threshold
     if qe_threshold > 0.0:
         filtered_train_data = dataset["train"].filter(
             lambda example: float(example["qe"]) >= qe_threshold
@@ -190,6 +226,8 @@ def create_dataset(
             }
         )
 
+    # Rename premise and hypothesis columns to not include a language,
+    # change label to labels.
     final_column_renames = {
         "premise_{}".format(language): "premise",
         "hypothesis_{}".format(language): "hypothesis",
@@ -198,6 +236,7 @@ def create_dataset(
 
     dataset = dataset.rename_columns(final_column_renames)
 
+    # Keep only the premise, hypothesis, labels and - if present - qe columns in the dataset
     select_column_names = ["premise", "hypothesis", "labels"]
     if language == "nl" and not baseline:
         select_column_names.append("qe")
@@ -205,15 +244,30 @@ def create_dataset(
     final_dataset = DatasetDict(
         {
             "train": dataset["train"].select_columns(select_column_names),
-            "validation": dataset["validation"].select_columns(["premise", "hypothesis", "labels"]),
+            "validation": dataset["validation"].select_columns(
+                ["premise", "hypothesis", "labels"]
+            ),
         }
     )
 
     return final_dataset
 
 
-def tokenize_data(dataset, tokenizer):
-    def tokenize_function(example):
+def tokenize_data(dataset: Dataset, tokenizer: PreTrainedTokenizer) -> Dataset:
+    """
+    Tokenizes the premise and hypothesis fields of the input data using the given tokenizer.
+    A [SEP] token will be added between the fields by the tokenizer, since this is expected by
+    BERT-based models.
+
+    Parameters:
+        dataset (Dataset): Dataset that contains a premise and hypothesis field
+        tokenizer (PreTrainedTokenizer): Insance of a pretrained tokenizer
+
+    Returns:
+        Dataset with tokenized input
+    """
+
+    def tokenize_function(example: dict) -> dict:
         return tokenizer(
             example["premise"],
             example["hypothesis"],
@@ -227,7 +281,35 @@ def tokenize_data(dataset, tokenizer):
 
 
 class TrainerWithQeWeights(Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
+    """
+    A custom trainer class that extends HuggingFace's Trainer to support
+    weighted loss computation based on quality estimation (QE) weights.
+
+    Methods:
+        compute_loss(model, inputs, return_outputs=False):
+            Computes the loss using QE weights. If QE weights are missing or invalid,
+            it returns the unweighted loss.
+    """
+
+    def compute_loss(
+        self,
+        model: PreTrainedModel,
+        inputs: dict[str, torch.Tensor],
+        return_outputs: bool = False,
+    ) -> Union[torch.Tensor, tuple[torch.Tensor, dict]]:
+        """
+        Computes the loss for a batch of inputs using QE scores as weights.
+
+        Parameters:
+            model (PreTrainedModel): The model being finetuned.
+            inputs (dict[str, torch.Tensor]): Inputs to the model, should have 'labels' and 'qe' column.
+            return_outputs (bool, optional): If True, the method also returns the model's outputs.
+                Defaults to False.
+
+        Returns:
+            Union[torch.Tensor, tuple[torch.Tensor, dict]]: The computed loss tensor or a tuple
+                of the loss and the model's outputs if return_outputs is set to True.
+        """
         labels = inputs.pop("labels")
         qe_weights = inputs.pop("qe")
         outputs = model(**inputs)
@@ -258,15 +340,33 @@ class TrainerWithQeWeights(Trainer):
 
 
 def train_model(
-    tokenized_dataset,
-    model_name="GroNLP/bert-base-dutch-cased",
-    nr_epochs=5,
-    weighted_loss=False,
-    batch_size=32,
-):
+    tokenized_dataset: DatasetDict,
+    model_name: str = "GroNLP/bert-base-dutch-cased",
+    nr_epochs: int = 5,
+    weighted_loss: bool = False,
+    batch_size: int = 32,
+) -> Trainer:
+    """
+    Trains a model on a given tokenized dataset for sequence classification.
+
+    Parameters:
+        tokenized_dataset (DatasetDict): A tokenized dataset. Should include 'train' and 'validation' splits.
+        model_name (str, optional): Modelname from HuggingFace's model hub. Defaults to "GroNLP/bert-base-dutch-cased".
+        nr_epochs (int, optional): Number of epochs to train the model for. Defaults to 5.
+        weighted_loss (bool, optional): Indicates whether to use qe-based weighted loss during training.
+            If True, requires qe scores in the dataset. Defaults to False.
+        batch_size (int, optional): Batch size for training and evaluation. Defaults to 32.
+
+    Returns:
+        Trainer: A trained Trainer with the fine-tuned model.
+    """
     metric = evaluate.load("accuracy", module_type="metric")
 
-    def compute_metrics(eval_pred):
+    def compute_metrics(eval_pred: tuple[np.ndarray, np.ndarray]) -> dict[str, float]:
+        """
+        Computes the evaluation metrics for the model's predictions.
+        metric should be defined within this function's scope.
+        """
         logits, labels = eval_pred
         predictions = np.argmax(logits, axis=-1)
         return metric.compute(predictions=predictions, references=labels)
@@ -277,6 +377,7 @@ def train_model(
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
+    # If weighted loss is used, the Trainer needs to keep the column containing the qe scores.
     remove_unused_columns = True
     if weighted_loss:
         remove_unused_columns = False
@@ -289,14 +390,15 @@ def train_model(
         evaluation_strategy="epoch",
         logging_dir="logs",
         output_dir="trainer",
-        save_strategy='no',
+        save_strategy="no",
         fp16=True,
         dataloader_drop_last=True,
         gradient_accumulation_steps=4,
         remove_unused_columns=remove_unused_columns,
     )
 
-    # Initialize trainer
+    # Initialize trainer. If weighted_loss is set to True, use a custom Trainer
+    # that supports weighted loss, otherwise use the standard Trainer.
     if weighted_loss:
         trainer = TrainerWithQeWeights(
             model=model.to(device),
@@ -320,30 +422,34 @@ def train_model(
 
 
 if __name__ == "__main__":
-    print('data check')
     args = create_arg_parser()
 
-    if args.language == "nl":
-        model_name = "GroNLP/bert-base-dutch-cased"
-    elif args.language == "en":
-        model_name = "google-bert/bert-base-cased"
-
+    # Create the (optionally filtered) dataset
     dataset = create_dataset(
         qe_threshold=args.qe_threshold,
         score_method=args.score_method,
         qe_mix_da_weight=args.qe_mix_da_weight,
         language=args.language,
-        baseline=args.baseline
+        baseline=args.baseline,
     )
 
     print(dataset)
 
+    # If check_data is not set to True, load and train model
     if not args.check_data:
+        # Load in the correct model based on the given language
+        if args.language == "nl":
+            model_name = "GroNLP/bert-base-dutch-cased"
+        elif args.language == "en":
+            model_name = "google-bert/bert-base-cased"
+
+        # Load in the correct tokenizer
         tokenizer = AutoTokenizer.from_pretrained(
             model_name, max_length=512, padding=True, truncation=True
         )
         tokenized_dataset = tokenize_data(dataset, tokenizer)
 
+        # Train the model
         trainer = train_model(
             tokenized_dataset,
             model_name=model_name,
@@ -352,6 +458,9 @@ if __name__ == "__main__":
             weighted_loss=args.weighted_loss,
         )
 
+        # If save_model is set to True, dynamically create a folder name based on
+        # the provided parameters to save the model in. If a save_folder is given,
+        # the model will be stored there (in a subfolder with the dynamically created folder name)
         if args.save_model:
             save_path = f"bert_{args.language}_e{args.nr_epochs}_b{args.batch_size}"
             if args.baseline:
@@ -362,7 +471,7 @@ if __name__ == "__main__":
                 save_path += f"_daweight{args.qe_mix_da_weight}"
             if args.weighted_loss:
                 save_path += "_wl"
-                
+
             if args.save_folder:
                 save_path = os.path.join(args.save_folder, save_path)
             trainer.save_model(save_path)
--- get_predictions.py
+++ get_predictions.py
@@ -1,11 +1,18 @@
-from transformers import AutoTokenizer, pipeline, AutoModelForSequenceClassification
-from datasets import load_dataset, DatasetDict
 import argparse
 import os
+
 import torch
+from datasets import Dataset, DatasetDict, load_dataset
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
 
-def create_arg_parser():
+def create_arg_parser() -> argparse.Namespace:
+    """
+    Creates and returns argument parser for command-line arguments.
+
+    Returns:
+        Namespace containing the command-line arguments as attributes.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-mf",
@@ -26,7 +33,15 @@ def create_arg_parser():
     return args
 
 
-def create_dataset():
+def create_dataset() -> DatasetDict:
+    """
+    Loads in the maximedb/sick_nl dataset from HuggingFace.
+
+    Returns:
+        DatasetDict: DatasetDict containing two Datasets containing the premise,
+            hypothesis and labels: 'nl' (Dutch) and 'en' (English)
+    """
+    # Load in the sick dataset and rename columns to better understandable names
     dataset_sicknl = load_dataset("maximedb/sick_nl")
     dataset_sicknl = dataset_sicknl.rename_columns(
         {
@@ -37,24 +52,43 @@ def create_dataset():
         }
     )
 
+    # Return dataset containing a Dutch and English split.
     return DatasetDict(
         {
             "nl": dataset_sicknl["test"].select_columns(
                 ["premise", "hypothesis", "label"]
             ),
-            "en": dataset_sicknl["test"].select_columns(
-                ["premise_en", "hypothesis_en", "label"]
-            ).rename_columns({"premise_en": "premise", "hypothesis_en": "hypothesis"}),
+            "en": dataset_sicknl["test"]
+            .select_columns(["premise_en", "hypothesis_en", "label"])
+            .rename_columns({"premise_en": "premise", "hypothesis_en": "hypothesis"}),
         }
     )
 
 
-def create_input_with_sep(example):
+def create_input_with_sep(example: dict) -> dict:
+    """
+    Concatenates the 'premise' and 'hypothesis' fields from a given example with
+    a '[SEP]' token for easier use in a Transformers pipeline. Intended to be used
+    on a DatasetDict using the map() method.
+    """
     concatenated_text = f"{example['premise']} [SEP] {example['hypothesis']}"
     return {"concatenated_input": concatenated_text}
 
 
-def decode_labels(predictions, label2id):
+def decode_labels(predictions: list[dict], label2id: dict) -> list[dict]:
+    """
+    Decodes prediction labels into their original integer representation. Also renames
+    the 'label' and 'score' columns to 'prediction' and 'confidence' to avoid mix-ups.
+
+    Parameters:
+        predictions (list[dict]): A list of dictionaries, each containing the keys 'label'
+            and 'score' (confidence score of the prediction).
+        label2id (dict): A dictionary mapping the labels as given by the model
+        to the original representations.
+
+    Returns:
+        list[dict]: A list of dictionaries, each containing the keys 'prediction' and 'confidence'.
+    """
     decoded_predictions = [
         {"prediction": label2id[prediction["label"]], "confidence": prediction["score"]}
         for prediction in predictions
@@ -62,54 +96,88 @@ def decode_labels(predictions, label2id):
     return decoded_predictions
 
 
-def get_predictions(dataset, model_name, tokenizer_name):
+def get_predictions(
+    dataset: Dataset, model_name: str, tokenizer_name: str
+) -> tuple[list, list]:
+    """
+    Loads in a tokenizer and sequence classification model and gets predictions
+    using Hugging Face's text-classification pipeline.
+
+    Parameters:
+        dataset (Dataset): A dataset containing the input data under the key 'concatenated_input'.
+        model_name (str): The name or path of the pretrained model to load.
+        tokenizer_name (str): The name or path of the tokenizer to load.
+
+    Returns:
+        tuple[list, list]: Two lists, one containing predictions and one containing confidence scores.
+    """
+    # Load in tokenizer
     tokenizer = AutoTokenizer.from_pretrained(
         tokenizer_name,
         max_length=512,
         padding=True,
         truncation=True,
     )
+
+    # Load in model
     model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=3)
+
+    # Create classifier pipeline, using gpu if available and get results
     device = 0 if torch.cuda.is_available() else -1
-    classifier = pipeline("text-classification", model, tokenizer=tokenizer, device=device)
+    classifier = pipeline(
+        "text-classification", model, tokenizer=tokenizer, device=device
+    )
     results = classifier(dataset["concatenated_input"])
 
+    # Get label mapping from model and decode labels
     label2id = model.config.label2id
-
     decoded_results = decode_labels(results, label2id)
 
-    predictions = [item['prediction'] for item in decoded_results]
-    confidence_scores = [item['confidence'] for item in decoded_results]
+    # Create two lists of predictions and confidence scores and return them
+    predictions = [item["prediction"] for item in decoded_results]
+    confidence_scores = [item["confidence"] for item in decoded_results]
 
     return predictions, confidence_scores
 
 
 if __name__ == "__main__":
-    print('test')
+    print("final check123")
     args = create_arg_parser()
     models_folder = args.model_folder
     output_folder = args.output_folder
 
+    # Create test dataset and concatenate premise and hypothesis using [SEP] as separator
     test_data = create_dataset().map(create_input_with_sep)
 
+    # Check if model and output folders exist
     if not os.path.isdir(models_folder):
         print(f"The path {models_folder} does not exist or is not a directory.")
+        if not os.path.isdir(output_folder):
+            print(f"The path {output_folder} does not exist or is not a directory.")
     else:
+        # Loop though models in model folder to get predictions
         for model_name in os.listdir(models_folder):
+            # Create full paths using model name
             full_path = os.path.join(models_folder, model_name)
-            output_path = os.path.join(output_folder, model_name+".csv")
+            output_path = os.path.join(output_folder, model_name + ".csv")
             output_data = None
             if model_name.startswith("bert_nl"):
+                # If the model is Dutch, use Dutch data to get predictions,
+                # save predictions in empty Dataset
                 predictions, confidence_scores = get_predictions(
                     test_data["nl"], full_path, "GroNLP/bert-base-dutch-cased"
                 )
                 output_data = test_data["nl"].add_column("predictions", predictions)
             elif model_name.startswith("bert_en"):
+                # If the model is English, use English data to get predictions,
+                # save predictions in empty Dataset
                 predictions, confidence_scores = get_predictions(
                     test_data["en"], full_path, "google-bert/bert-base-cased"
                 )
                 output_data = test_data["en"].add_column("predictions", predictions)
+            # Add confidence scores to dataset with predictions and remove concatenated_input
             output_data = output_data.add_column("confidence", confidence_scores)
-            output_data.remove_columns("concatenated_input")
+            output_data = output_data.remove_columns("concatenated_input")
+            # Save final dataset original data and predictions and confidence scores to csv file.
             output_data.to_csv(output_path)
             print("Predictions saved as {}".format(output_path))
